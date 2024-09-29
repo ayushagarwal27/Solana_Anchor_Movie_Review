3c926414e57adcac784e8dab61b1abdb62e7ec23
Ayush <ayushagarwal.dev@gmail.com>
2024-09-29 22:04:27 +0530

    add movie state and initialize pda struct, add add_movie_review function

---
+++ programs/anchor-movie-review-program/src/lib.rs
@@ -0,0 +1,72 @@
+use anchor_lang::prelude::*;
+
+declare_id!("6NA441G9Z58G7FNxqjSUDbRpScPF9KzQQEHfDCe9GhqS");
+
+const DISCRIMINATOR: usize = 8;
+const MIN_RATING: u8 = 1;
+const MAX_RATING: u8 = 5;
+const MAX_TITLE_LENGTH: usize = 20;
+const MAX_DESCRIPTION_LENGTH: usize = 50;
+
+// #[program]
+pub mod anchor_movie_review_program {
+    use super::*;
+
+    pub fn add_movie_review(ctx: Context<AddMovieReview>, title: String, description: String, rating: u8) -> Result<()> {
+        require!(rating >= MIN_RATING && rating <= MAX_RATING, MovieReviewError::InvalidRating);
+        require!(title.len() <= MAX_TITLE_LENGTH, MovieReviewError::TitleTooLong);
+        require!(description.len() <= MAX_DESCRIPTION_LENGTH, MovieReviewError::DescriptionTooLong);
+
+        msg!("Movie Review Account Created");
+        msg!("Title: {}", title);
+        msg!("Description: {}", description);
+        msg!("Rating: {}", rating);
+
+        let moview_review = &mut ctx.accounts.movie_review;
+        moview_review.title = title;
+        moview_review.description = description;
+        moview_review.rating = rating;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(title:String, description:String)]
+pub struct AddMovieReview<'info> {
+    #[account(
+        init,
+        seeds=[title.as_bytes(), initializer.key().as_ref]
+        bump,
+        payer = initializer,
+        space =DISCRIMINATOR + MovieAccountState::INIT_SPACE
+    )]
+    pub movie_review: Account<'info, MovieAccountState>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+
+#[account]
+#[derive(InitSpace)]
+pub struct MovieAccountState {
+    pub reviewer: Pubkey,
+    pub rating: u8,
+    #[max_len(20)]
+    pub title: String,
+    #[max_len(50)]
+    pub description: String,
+}
+
+#[error_code]
+enum MovieReviewError {
+    #[msg("Rating must be between 1 to 5")]
+    InvalidRating,
+    #[msg("Movie Title too long")]
+    TitleTooLong,
+    #[msg("Movie Description too long")]
+    DescriptionTooLong,
+}
+
+
+
